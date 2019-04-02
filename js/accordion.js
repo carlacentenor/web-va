a127af7be323bdda8a8f85f4d3e1d30e697905b5
carlacentenor <carlacentenor@gmail.com>
2019-04-02 17:26:11 -0500

    add text in accordion

--- js/accordion.js
+++ js/accordion.js
@@ -13,75 +13,160 @@ let title4 = document.querySelector(".title4-section-js");
 let navMenu = document.querySelector(".align-nav-menu-js");
 
 
+section1.addEventListener("click", () => {
+    let val1 = section1.getAttribute("data-value");
 
-section1.addEventListener('click', () => {
-    expand(section1, contentSection1);
-    remove(section2);
-    hideContent(contentSection2);
-    hideContent(contentSection3);
-    hideContent(contentSection4);
-    remove(section3);
-    remove(section4);
-    navMenu.classList.remove('w25');
-    navMenu.classList.add('w5');
-})
-section2.addEventListener('click', () => {
-    expand(section2, contentSection2);
-    remove(section1);
-    remove(section3);
-    remove(section4);
-    hideContent(contentSection1);
-    hideContent(contentSection3);
-    hideContent(contentSection4);
-    navMenu.classList.remove('w25');
-    navMenu.classList.add('w5');
-
-
-})
-section3.addEventListener('click', () => {
-    expand(section3, contentSection3);
-    remove(section1);
-    remove(section2);
-    remove(section4);
-    hideContent(contentSection2);
-    hideContent(contentSection1);
-    hideContent(contentSection4);
-    navMenu.classList.remove('w25');
-    navMenu.classList.add('w5');
-
-
-})
-section4.addEventListener('click', () => {
-    expand(section4, contentSection4);
-    remove(section1);
-    remove(section3);
-    remove(section2);
-    hideContent(contentSection2);
-    hideContent(contentSection3);
-    hideContent(contentSection1);
-    navMenu.classList.remove('w5');
-    navMenu.classList.add('w25');
-
-
-})
+    if (val1 == "1") {
+        section1.setAttribute("data-value", "0");
+        section2.setAttribute("data-value", "1");
+        section3.setAttribute("data-value", "1");
+        section4.setAttribute("data-value", "1");
+        remove(section2);
+        remove(section3);
+        remove(section4);
+        expandadd(section1, contentSection1);
+        hideContent(contentSection2);
+        hideContent(contentSection3);
+        hideContent(contentSection4);
+        hideContent(title1);
+        if (section2.getAttribute("data-value") == "1") {
+            showContent(title2);
+        }
+        if (section3.getAttribute("data-value") == "1") {
+            showContent(title3);
+        }
+        if (section4.getAttribute("data-value") == "1") {
+            showContent(title4);
+        }
+    } else {
+        section1.setAttribute("data-value", "1");
+        expandhide(section1, contentSection1);
+        showContent(title1);
+    }
+});
+section2.addEventListener("click", () => {
+    let val2 = section2.getAttribute("data-value");
+    if (val2 == "1") {
+        section1.setAttribute("data-value", "1");
+        section2.setAttribute("data-value", "0");
+        section3.setAttribute("data-value", "1");
+        section4.setAttribute("data-value", "1");
+        remove(section1);
+        remove(section3);
+        remove(section4);
+        if (section4.getAttribute("data-value") == "1") {
+            showContent(title4);
+        }
+        if (section3.getAttribute("data-value") == "1") {
+            showContent(title3);
+        }
+        if (section1.getAttribute("data-value") == "1") {
+            showContent(title1);
+        }
+        expandadd(section2, contentSection2);
+        hideContent(title2);
+        hideContent(contentSection3);
+        hideContent(contentSection1);
+        hideContent(contentSection4);
+    } else {
+        section2.setAttribute("data-value", "1");
+        expandhide(section2, contentSection2);
+        showContent(title2);
+    }
+});
+section3.addEventListener("click", () => {
+    let val3 = section3.getAttribute("data-value");
+    if (val3 == "1") {
+        section1.setAttribute("data-value", "1");
+        section2.setAttribute("data-value", "1");
+        section3.setAttribute("data-value", "0");
+        section4.setAttribute("data-value", "1");
+        remove(section1);
+        remove(section2);
+        remove(section4);
+        expandadd(section3, contentSection3);
+        hideContent(contentSection2);
+        hideContent(contentSection1);
+        hideContent(contentSection4);
+        hideContent(title3);
+        if (section2.getAttribute("data-value") == "1") {
+            showContent(title2);
+        }
+        if (section4.getAttribute("data-value") == "1") {
+            showContent(title4);
+        }
+        if (section1.getAttribute("data-value") == "1") {
+            showContent(title1);
+        }
+    } else {
+        section3.setAttribute("data-value", "1");
+        expandhide(section3, contentSection3);
+        showContent(title3);
+    }
+});
+section4.addEventListener("click", () => {
+    let val4 = section4.getAttribute("data-value");
+    if (val4 == "1") {
+        section1.setAttribute("data-value", "1");
+        section2.setAttribute("data-value", "1");
+        section3.setAttribute("data-value", "1");
+        section4.setAttribute("data-value", "0");
+        remove(section1);
+        remove(section3);
+        remove(section2);
+        expandadd(section4, contentSection4);
+        hideContent(contentSection2);
+        hideContent(contentSection1);
+        hideContent(contentSection3);
+        hideContent(title4);
+        if (section2.getAttribute("data-value") == "1") {
+            showContent(title2);
+        }
+        if (section3.getAttribute("data-value") == "1") {
+            showContent(title3);
+        }
+        if (section1.getAttribute("data-value") == "1") {
+            showContent(title1);
+        }
+    } else {
+        section4.setAttribute("data-value", "1");
+        expandhide(section4, contentSection4);
+        showContent(title4);
+    }
+});
 
 const expand = (element, elementContent) => {
     element.classList.toggle("expand-section");
     element.classList.toggle("bg-white");
-    elementContent.classList.toggle('hide');
+    elementContent.classList.toggle("hide");
+};
 
-}
+const expandadd = (element, elementContent) => {
+    element.classList.add("expand-section");
+    element.classList.add("bg-white");
+    elementContent.classList.remove("hide");
+};
 
-const remove = (element) => {
+const expandhide = (element, elementContent) => {
     element.classList.remove("expand-section");
-    element.classList.remove("bg-white");;
-}
+    element.classList.remove("bg-white");
+    elementContent.classList.add("hide");
+};
 
-const hideContent = (element) => {
+const remove = element => {
+    element.classList.remove("expand-section");
+    element.classList.remove("bg-white");
+};
+
+
+const hideContent = element => {
     element.classList.add("hide");
+};
 
-}
+const showContent = element => {
+    element.classList.remove("hide");
+};
 
-const showContent = (element) => {
-    element.classList.add("show");
-}
+const toggle = element => {
+    element.classList.toggle("hide");
+};
