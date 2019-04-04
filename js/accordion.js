53892537888aaf4f9f02b20dea9066dcb887cd7d
carlacentenor <carlacentenor@gmail.com>
2019-04-04 12:20:14 -0500

    add navbar fixed process

--- js/accordion.js
+++ js/accordion.js
@@ -12,161 +12,201 @@ let title3 = document.querySelector(".title3-section-js");
 let title4 = document.querySelector(".title4-section-js");
 let navMenu = document.querySelector(".align-nav-menu-js");
 
+let changeColorNavBorder = document.querySelector(".change-color-b");
+let changeColorNavLines = document.querySelector(".change-color-l");
+let logoWhite = document.querySelector(".logo-white");
+let logoTur = document.querySelector(".logo-tur");
 
 section1.addEventListener("click", () => {
-    let val1 = section1.getAttribute("data-value");
+  let val1 = section1.getAttribute("data-value");
 
-    if (val1 == "1") {
-        section1.setAttribute("data-value", "0");
-        section2.setAttribute("data-value", "1");
-        section3.setAttribute("data-value", "1");
-        section4.setAttribute("data-value", "1");
-        remove(section2);
-        remove(section3);
-        remove(section4);
-        expandadd(section1, contentSection1);
-        hideContent(contentSection2);
-        hideContent(contentSection3);
-        hideContent(contentSection4);
-        hideContent(title1);
-        if (section2.getAttribute("data-value") == "1") {
-            showContent(title2);
-        }
-        if (section3.getAttribute("data-value") == "1") {
-            showContent(title3);
-        }
-        if (section4.getAttribute("data-value") == "1") {
-            showContent(title4);
-        }
-    } else {
-        section1.setAttribute("data-value", "1");
-        expandhide(section1, contentSection1);
-        showContent(title1);
+  if (val1 == "1") {
+    section1.setAttribute("data-value", "0");
+    section2.setAttribute("data-value", "1");
+    section3.setAttribute("data-value", "1");
+    section4.setAttribute("data-value", "1");
+    remove(section2);
+    remove(section3);
+    remove(section4);
+    expandadd(section1, contentSection1);
+    hideContent(contentSection2);
+    hideContent(contentSection3);
+    hideContent(contentSection4);
+    hideContent(title1);
+    changeColorNavBarIconsClose();
+    hideContent(logoWhite);
+    showContent(logoTur);
+    
+    if (section2.getAttribute("data-value") == "1") {
+      showContent(title2);
     }
+    if (section3.getAttribute("data-value") == "1") {
+      showContent(title3);
+    }
+    if (section4.getAttribute("data-value") == "1") {
+      showContent(title4);
+    }
+  } else {
+    section1.setAttribute("data-value", "1");
+    expandhide(section1, contentSection1);
+    showContent(title1);
+    hideContent(logoTur);
+    showContent(logoWhite);
+  }
 });
 section2.addEventListener("click", () => {
-    let val2 = section2.getAttribute("data-value");
-    if (val2 == "1") {
-        section1.setAttribute("data-value", "1");
-        section2.setAttribute("data-value", "0");
-        section3.setAttribute("data-value", "1");
-        section4.setAttribute("data-value", "1");
-        remove(section1);
-        remove(section3);
-        remove(section4);
-        if (section4.getAttribute("data-value") == "1") {
-            showContent(title4);
-        }
-        if (section3.getAttribute("data-value") == "1") {
-            showContent(title3);
-        }
-        if (section1.getAttribute("data-value") == "1") {
-            showContent(title1);
-        }
-        expandadd(section2, contentSection2);
-        hideContent(title2);
-        hideContent(contentSection3);
-        hideContent(contentSection1);
-        hideContent(contentSection4);
-    } else {
-        section2.setAttribute("data-value", "1");
-        expandhide(section2, contentSection2);
-        showContent(title2);
+  let val2 = section2.getAttribute("data-value");
+  if (val2 == "1") {
+    section1.setAttribute("data-value", "1");
+    section2.setAttribute("data-value", "0");
+    section3.setAttribute("data-value", "1");
+    section4.setAttribute("data-value", "1");
+    remove(section1);
+    remove(section3);
+    remove(section4);
+    changeColorNavBarIconsClose();
+    hideContent(logoTur);
+    showContent(logoWhite);
+    if (section4.getAttribute("data-value") == "1") {
+      showContent(title4);
     }
+    if (section3.getAttribute("data-value") == "1") {
+      showContent(title3);
+    }
+    if (section1.getAttribute("data-value") == "1") {
+      showContent(title1);
+    }
+    expandadd(section2, contentSection2);
+    hideContent(title2);
+    hideContent(contentSection3);
+    hideContent(contentSection1);
+    hideContent(contentSection4);
+  } else {
+    section2.setAttribute("data-value", "1");
+    expandhide(section2, contentSection2);
+    showContent(title2);
+  }
 });
 section3.addEventListener("click", () => {
-    let val3 = section3.getAttribute("data-value");
-    if (val3 == "1") {
-        section1.setAttribute("data-value", "1");
-        section2.setAttribute("data-value", "1");
-        section3.setAttribute("data-value", "0");
-        section4.setAttribute("data-value", "1");
-        remove(section1);
-        remove(section2);
-        remove(section4);
-        expandadd(section3, contentSection3);
-        hideContent(contentSection2);
-        hideContent(contentSection1);
-        hideContent(contentSection4);
-        hideContent(title3);
-        if (section2.getAttribute("data-value") == "1") {
-            showContent(title2);
-        }
-        if (section4.getAttribute("data-value") == "1") {
-            showContent(title4);
-        }
-        if (section1.getAttribute("data-value") == "1") {
-            showContent(title1);
-        }
-    } else {
-        section3.setAttribute("data-value", "1");
-        expandhide(section3, contentSection3);
-        showContent(title3);
+  let val3 = section3.getAttribute("data-value");
+  if (val3 == "1") {
+    section1.setAttribute("data-value", "1");
+    section2.setAttribute("data-value", "1");
+    section3.setAttribute("data-value", "0");
+    section4.setAttribute("data-value", "1");
+    remove(section1);
+    remove(section2);
+    remove(section4);
+    expandadd(section3, contentSection3);
+    hideContent(contentSection2);
+    hideContent(contentSection1);
+    hideContent(contentSection4);
+    hideContent(title3);
+    changeColorNavBarIconsClose();
+    hideContent(logoTur);
+    showContent(logoWhite);
+    if (section2.getAttribute("data-value") == "1") {
+      showContent(title2);
     }
+    if (section4.getAttribute("data-value") == "1") {
+      showContent(title4);
+    }
+    if (section1.getAttribute("data-value") == "1") {
+      showContent(title1);
+    }
+  } else {
+    section3.setAttribute("data-value", "1");
+    expandhide(section3, contentSection3);
+    showContent(title3);
+  }
 });
 section4.addEventListener("click", () => {
-    let val4 = section4.getAttribute("data-value");
-    if (val4 == "1") {
-        section1.setAttribute("data-value", "1");
-        section2.setAttribute("data-value", "1");
-        section3.setAttribute("data-value", "1");
-        section4.setAttribute("data-value", "0");
-        remove(section1);
-        remove(section3);
-        remove(section2);
-        expandadd(section4, contentSection4);
-        hideContent(contentSection2);
-        hideContent(contentSection1);
-        hideContent(contentSection3);
-        hideContent(title4);
-        if (section2.getAttribute("data-value") == "1") {
-            showContent(title2);
-        }
-        if (section3.getAttribute("data-value") == "1") {
-            showContent(title3);
-        }
-        if (section1.getAttribute("data-value") == "1") {
-            showContent(title1);
-        }
-    } else {
-        section4.setAttribute("data-value", "1");
-        expandhide(section4, contentSection4);
-        showContent(title4);
+  let val4 = section4.getAttribute("data-value");
+  if (val4 == "1") {
+    section1.setAttribute("data-value", "1");
+    section2.setAttribute("data-value", "1");
+    section3.setAttribute("data-value", "1");
+    section4.setAttribute("data-value", "0");
+    remove(section1);
+    remove(section3);
+    remove(section2);
+    expandadd(section4, contentSection4);
+    hideContent(contentSection2);
+    hideContent(contentSection1);
+    hideContent(contentSection3);
+    hideContent(title4);
+
+    changeColorNavBarIconsOpen();
+
+    hideContent(logoTur);
+    showContent(logoWhite);
+    if (section2.getAttribute("data-value") == "1") {
+      showContent(title2);
+
+
     }
+    if (section3.getAttribute("data-value") == "1") {
+      showContent(title3);
+
+
+    }
+    if (section1.getAttribute("data-value") == "1") {
+      showContent(title1);
+
+
+    }
+  } else {
+    section4.setAttribute("data-value", "1");
+    expandhide(section4, contentSection4);
+    showContent(title4);
+    changeColorNavBarIconsClose();
+  }
 });
 
 const expand = (element, elementContent) => {
-    element.classList.toggle("expand-section");
-    element.classList.toggle("bg-white");
-    elementContent.classList.toggle("hide");
+  element.classList.toggle("expand-section");
+  element.classList.toggle("bg-white");
+  elementContent.classList.toggle("hide");
 };
 
 const expandadd = (element, elementContent) => {
-    element.classList.add("expand-section");
-    element.classList.add("bg-white");
-    elementContent.classList.remove("hide");
+  element.classList.add("expand-section");
+  element.classList.add("bg-white");
+  elementContent.classList.remove("hide");
 };
 
 const expandhide = (element, elementContent) => {
-    element.classList.remove("expand-section");
-    element.classList.remove("bg-white");
-    elementContent.classList.add("hide");
+  element.classList.remove("expand-section");
+  element.classList.remove("bg-white");
+  elementContent.classList.add("hide");
 };
 
 const remove = element => {
-    element.classList.remove("expand-section");
-    element.classList.remove("bg-white");
+  element.classList.remove("expand-section");
+  element.classList.remove("bg-white");
 };
 
 
 const hideContent = element => {
-    element.classList.add("hide");
+  element.classList.add("hide");
 };
 
 const showContent = element => {
-    element.classList.remove("hide");
+  element.classList.remove("hide");
 };
 
 const toggle = element => {
-    element.classList.toggle("hide");
+  element.classList.toggle("hide");
 };
+
+const changeColorNavBarIconsOpen = () => {
+  changeColorNavBorder.classList.remove('hamburger__border--only-w');
+  changeColorNavBorder.classList.add('hamburger__border--blue');
+  changeColorNavLines.classList.remove('hamburger-inner-only-white');
+};
+
+const changeColorNavBarIconsClose = () => {
+  changeColorNavBorder.classList.add('hamburger__border--only-w');
+  changeColorNavBorder.classList.remove('hamburger__border--blue');
+  changeColorNavLines.classList.add('hamburger-inner-only-white');
+}
--- js/app.js
+++ js/app.js
@@ -1,9 +1,12 @@
 const hamburger = document.querySelector(".hamburger");
 const menuOpen = document.querySelector(".open-menu-js");
-const menuClose = document.querySelector(".close-menu-js")
+// const menuOpenAcordion = document.querySelector(".open-menu-acordion-js");
+const menuClose = document.querySelector(".close-menu-js");
+// const menuCloseAcordion = document.querySelector(".close-menu--accordion-js");
 const expandMenu = document.querySelector(".expand-js");
+// const expandMenuAcordion = document.querySelector(".expand-accordion-js");
 const menuItem = document.querySelector('.content-menu-js');
-
+// const menuItemAcordion = document.querySelector('.content-menu-accordion-js');
 const routeWork = document.querySelector('.item-work-js');
 const routeAbout = document.querySelector('.item-about-js');
 const routeServices = document.querySelector('.item-services-js');
@@ -11,8 +14,7 @@ const routeContact = document.querySelector('.item-contact-js');
 const routeHome = document.querySelector('.item-home-js');
 const btnTranslateMobile = document.querySelector('.btn-translate');
 const btnTranslateDesktop = document.querySelector('.btn-translate-d');
-const translateEn = document.getElementsByClassName('translate-en');
-const translateEs = document.getElementsByClassName('translate-es');
+
 // Open Menu
 
 
@@ -24,6 +26,13 @@ menuOpen.addEventListener("click", () => {
 
 });
 
+// menuOpenAcordion.addEventListener("click", () => {
+//     expandMenuAcordion.classList.add('menu-section-expand');
+//     expandMenuAcordion.classList.remove('hide');
+//     viewOptionAcordion();
+
+// });
+
 menuClose.addEventListener("click", () => {
     expandMenu.classList.remove('menu-section-expand');
     expandMenu.classList.add('hide');
@@ -31,10 +40,21 @@ menuClose.addEventListener("click", () => {
 })
 
 
+// menuCloseAcordion.addEventListener("click", () => {
+//     expandMenuAcordion.classList.remove('menu-section-expand');
+//     expandMenuAcordion.classList.add('hide');
+//     hideOption();
+// })
+
 function viewOption() {
     menuItem.classList.remove('hide');
     menuItem.classList.add('menu-item');
 }
+
+// function viewOptionAcordion() {
+//     menuItemAcordion.classList.remove('hide');
+//     menuItemAcordion.classList.add('menu-item');
+// }
 
 function hideOption() {
     menuItem.classList.add('hide');
@@ -42,76 +62,44 @@ function hideOption() {
 }
 
 
-function translate() {
-
-    let valueLanguage = getLanguage();
-    if (valueLanguage == "es") {
-        for (var i = 0; i < translateEn.length; i++) {
-            translateEn[i].classList.remove("hide");
-        }
-        for (var i = 0; i < translateEs.length; i++) {
-            translateEs[i].classList.add("hide");
-        }
-        localStorage.setItem('language', 'en');
-    } else {
-        for (var i = 0; i < translateEn.length; i++) {
-            translateEn[i].classList.add("hide");
-        }
-        for (var i = 0; i < translateEs.length; i++) {
-            translateEs[i].classList.remove("hide");
-        }
-        localStorage.setItem('language', 'es');
-    }
+// function hideOptionAcordion() {
+//     menuItemAcordion.classList.add('hide');
+//     menuItemAcordion.classList.remove('menu-item');
+// }
 
 
 
-}
 
-function getLanguage() {
-    let language = localStorage.getItem('language');
-    return language;
 
-}
-
-btnTranslateMobile.addEventListener('click', () => {
-
-    translate();
-
-});
-
-btnTranslateDesktop.addEventListener('click', () => {
-    translate();
-
-});
 
 // Route
+
+
 routeWork.addEventListener('click', () => {
 
     window.location.href = '../views/proyect.html';
-    translate();
+
 
 })
 
 routeAbout.addEventListener('click', () => {
     window.location.href = '../views/about.html';
-    translate();
+ 
 
 })
 
 routeContact.addEventListener('click', () => {
     window.location.href = '../views/contact.html';
-    translate();
+    
 
 })
 
 routeServices.addEventListener('click', () => {
     window.location.href = '../views/process.html';
-    translate();
+   
 })
 
 routeHome.addEventListener('click', () => {
     window.location.href = '../views/home.html';
-    translate();
+    
 });
-
-translate();
